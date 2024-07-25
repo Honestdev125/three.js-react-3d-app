0ac641cb23beffe2c3dd50a407723b3e9a6a7033
TOP-DEV <successdev777@gmail.com>
2024-07-26 04:02:07 +0900

    fix:contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -118,7 +118,7 @@ const Contact = () => {
       </motion.div>
 
       <motion.div
-        variants={slideIn("right", "tween", 0.2, 1)}
+        variants={ slideIn("right", "tween", 0.2, 1) }
         className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
       >
         <EarthCanvas />
