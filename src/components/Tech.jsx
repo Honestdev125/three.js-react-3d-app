c9bc238b6b3d7d2dceccd320da6eee16788d698d
weijiezhang-star <144591405+weijiezhang-star@users.noreply.github.com>
2024-07-30 20:52:39 +0900

    changed tech component

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -8,7 +8,7 @@ const Tech = () => {
       <div className="flex flex-row flex-wrap justify-center gap-10">
         {technologies.map((technology) => (
           <div className="w-28 h-28" key={technology.name}>
-            <BallCanvas icon={technology.icon} />
+            <BallCanvas icon={technology.icon}/>
           </div>
         ))}
       </div>
